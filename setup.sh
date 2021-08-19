009f2c06b807433e43e1160b5b6b09f9c7a3c4d4
EXcaliburQAQ <1150790988@qq.com>
2021-08-20 07:53:23 +0800

    修改docker 命令

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ git pull --rebase origin main;
 #docker rm projecttestdocker;
 docker build -t projecttestdocker:$image_version .;
 docker images;
-docker run -p 3000:3000 -d --name projecttestdocker projecttestdocker:$image_version;
+docker run -p 3000:3000 -d --name=projecttestdocker projecttestdocker:$image_version;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
 docker logs projecttestdocker;
 #删除build过程中产生的镜像    #docker image prune -a -f
